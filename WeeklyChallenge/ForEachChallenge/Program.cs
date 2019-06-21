d3c62763227020fe836acf73112f3a7405b1cefc
Ville Kähärä <kahara.ville@gmail.com>
2019-06-21 07:47:20 +0300

    added blank solution and basic foreach challenge completed

---
+++ WeeklyChallenge/ForEachChallenge/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+/* Todo:
+ * Create a console app
+ * Create a list of strings
+ * Loop trough the list
+ * Print every name to the console
+ */
+
+//This program print set of names to the console.
+
+namespace ForEachChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<string> nameList = new List<string>() { "Bob Bobby", "Ray Ray", "Jack Box", "Tester Surname", "Wade Holton" };
+
+            foreach (string name in nameList)
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadLine();
+        }
+    }
+}
