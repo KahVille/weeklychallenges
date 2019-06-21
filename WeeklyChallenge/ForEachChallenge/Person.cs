132ee5c84a3b59c661620d7496640c160a63a982
Ville Kähärä <kahara.ville@gmail.com>
2019-06-21 08:29:12 +0300

    ForEachChallenge Bonus with property

---
+++ WeeklyChallenge/ForEachChallenge/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+//Bonus: use class intead of strings and print these to the console
+
+namespace ForEachChallenge
+{
+    class Person
+    {
+
+        public Person(string firstName = null, string lastName= null)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        private string firstName;
+        private string lastName;
+
+        public string Name => $"{firstName} {lastName}";
+
+    }
+}
--- WeeklyChallenge/ForEachChallenge/Program.cs
+++ WeeklyChallenge/ForEachChallenge/Program.cs
@@ -18,12 +18,30 @@ namespace ForEachChallenge
         static void Main(string[] args)
         {
 
+            //Goal
             List<string> nameList = new List<string>() { "Bob Bobby", "Ray Ray", "Jack Box", "Tester Surname", "Wade Holton" };
 
             foreach (string name in nameList)
             {
                 Console.WriteLine(name);
             }
+
+
+            //Bonus
+            Console.WriteLine("\nBonus: \n");
+            List<Person> peopleList = new List<Person>()
+            {
+                new Person("Mike","Magick"),
+                new Person("Helen","Magick"),
+                new Person("Tester","Surname"),
+                new Person("Wade","Box")
+            };
+
+            foreach(Person person in peopleList)
+            {
+                Console.WriteLine("Hello, My name is " + person.Name);
+            }
+
 
             Console.WriteLine("Press any key to continue");
             Console.ReadLine();
