4f12d3affd97204cfb4a9cd99e4af2e14439059e
Ville Kähärä <kahara.ville@gmail.com>
2019-06-22 10:06:54 +0300

    Added basic start point for the TextFile challenge

---
+++ WeeklyChallenge/TextFilesChallenge/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextFilesChallenge
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void AddUserButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddUser();
+        }
+
+        private void AddUser()
+        {
+
+        }
+
+        private void SaveListButton_Click(object sender, RoutedEventArgs e)
+        {
+            SaveList();
+        }
+
+        private void SaveList()
+        {
+         
+        }
+
+        private void UserDataListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void FirstNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void LastNameTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void AgeTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void IsAliveCheckBox_Click(object sender, RoutedEventArgs e)
+        {
+    
+        }
+    }
+
+}
