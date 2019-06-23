e6ca28e2bcca8b867f4768c530d698400515c568
Ville Kähärä <kahara.ville@gmail.com>
2019-06-23 10:18:21 +0300

    TextFileChallenge: first iteration of file read.
    displays the listbox element based on the given dataset

--- WeeklyChallenge/TextFilesChallenge/MainWindow.xaml.cs
+++ WeeklyChallenge/TextFilesChallenge/MainWindow.xaml.cs
@@ -1,6 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -20,11 +25,92 @@ namespace TextFilesChallenge
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            //assigns the list and sets display data provided from function.
+            UserDataListBox.ItemsSource = SetUpUserDataList();
+            UserDataListBox.DisplayMemberPath = "DisplayText";
+
         }
 
+
+        private ObservableCollection<UserModel> SetUpUserDataList()
+        {
+
+            string fileToLoad = "StandardDataSet.csv";
+            ParseDataSetFile(fileToLoad);
+
+            return ParseDataSetFile(fileToLoad);
+        }
+
+        private ObservableCollection<UserModel> ParseDataSetFile(string filename = null)
+        {
+
+            ObservableCollection<UserModel> userDataList = new ObservableCollection<UserModel>();
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            string resourceName = assembly.GetManifestResourceNames()
+                .Single(str => str.EndsWith(filename));
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string[] fieldNames = reader.ReadLine().Split(',');
+
+                string[] data = reader.ReadToEnd().Split('\n').ToArray();
+
+                if(string.IsNullOrWhiteSpace(data[data.Length-1]))
+                {
+                    data = data.Take(data.Length - 1).ToArray();
+                }
+
+                for (int index =0; index < data.Length; index++)
+                {
+
+                    string values = data[index];
+
+                    string[] dataValues = values.Split(',');
+
+                    UserModel newUserData = new UserModel();
+
+                    for (int jindex = 0; jindex < dataValues.Length; jindex++)
+                    {
+                        Debug.WriteLine($"{fieldNames[jindex]} {dataValues[jindex]} ");
+
+                        PropertyInfo propertyInfo = newUserData.GetType().GetProperty(fieldNames[jindex]);
+
+                       var converter = TypeDescriptor.GetConverter(propertyInfo.PropertyType);
+
+                    //from string to boolean special case
+                     if (propertyInfo.PropertyType == typeof(bool))
+                        {
+                            bool booleanValue = false;
+                            int number = int.Parse(dataValues[jindex]);
+                            if (number == 1)
+                            {
+                                booleanValue = true;
+                            }
+
+                            newUserData.GetType().GetProperty(fieldNames[jindex]).SetValue(newUserData, booleanValue, null);
+                        }
+                       else
+                        {
+                            var result = converter.ConvertFrom(dataValues[jindex]);
+                            newUserData.GetType().GetProperty(fieldNames[jindex]).SetValue(newUserData, result, null);
+
+                        }
+
+                    }
+                    userDataList.Add(newUserData);
+                }
+
+                return userDataList;
+
+            }
+
+        }
 
         private void AddUserButton_Click(object sender, RoutedEventArgs e)
         {
---
+++ WeeklyChallenge/TextFilesChallenge/docs/References.md
@@ -0,0 +1,7 @@
+﻿#	References
+These references were used during programming:
+
+*	[WPF Data binding](http://wangxinliu.com/tech/program/WPF-DataBinding/)
+*	[Optimizing Performance: Data Binding](https://docs.microsoft.com/en-us/dotnet/framework/wpf/advanced/optimizing-performance-data-binding)
+*	[Read emdedded resource file](https://stackoverflow.com/questions/3314140/how-to-read-embedded-resource-text-file)
+*	[Property Type](https://docs.microsoft.com/en-us/dotnet/api/system.reflection.propertyinfo.propertytype?view=netframework-4.8#System_Reflection_PropertyInfo_PropertyType)
